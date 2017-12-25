2e9f33eef34c569246ddd63bf485f884cf608913
ayushboss <byteSlayer31037@gmail.com>
2017-12-24 20:33:15 -0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+
+description = "Baylor-Bot"
+bot_prefix = "!"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print("Logged in");
+	print("Name: " + client.user.name);
+	print("ID: " + client.user.id);
+	print("\n\n");
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('!help'):
+		await client.send_message(message.channel, "HELP")
+
+client.run("Mzk0NjY3NjMyMzk1MDI2NDQz.DSHrWQ.YkIOkrzE3G-C7eTAUoOYbPAS8hw");
