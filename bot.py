4aced5d140c42df7d2139d94f7381a91c06edf2d
Ayush Suresh <byteSlayer31037@gmail.com>
2019-04-07 12:11:41 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,4 +18,4 @@ async def on_message(message):
 	if message.content.startswith('!help'):
 		await client.send_message(message.channel, "HELP")
 
-client.run("Mzk0NjY3NjMyMzk1MDI2NDQz.DSHrWQ.YkIOkrzE3G-C7eTAUoOYbPAS8hw");
+client.run("-- REDACTED --"); 
